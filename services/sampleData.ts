247d7b3fcce8cb5098ce8f5f8e9f5836a79916e9
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-01 12:56:30 +0200

    Update sampleData.ts

--- services/sampleData.ts
+++ services/sampleData.ts
@@ -3,92 +3,94 @@
 import { Person } from '../types';
 
 export const samplePeople: Person[] = [
+  // ─────────────────────────────
   // Generation 1 – Stammeltern
-  { id: '1', code: '1', name: 'Karl Geppert', birthDate: '1925-05-12', parentId: null, partnerId: '2' },
-  { id: '2', code: '1x', name: 'Anna Müller-Geppert', birthDate: '1928-09-03', parentId: null, partnerId: '1' },
+  // ─────────────────────────────
+  { id: 'p1',  code: '1',  name: 'Wilhelm Geppert', gender: 'm', birthDate: '1930-04-18', birthPlace: 'Berlin',    deathDate: '2012-03-10', parentId: null, partnerId: 'p2', hasRing: false, ringCode: 'R01' },
+  { id: 'p2',  code: '1x', name: 'Marta Schulz-Geppert', gender: 'f', birthDate: '1932-09-07', birthPlace: 'Leipzig', deathDate: '2018-01-26', parentId: null, partnerId: 'p1', hasRing: true,  ringCode: 'R11' },
 
-  // Generation 2 – Kinder
-  { id: '3', code: '1.1', name: 'Hans Geppert', birthDate: '1950-03-14', parentId: '1', partnerId: '4' },
-  { id: '4', code: '1.1x', name: 'Maria Schneider-Geppert', birthDate: '1952-11-21', parentId: null, partnerId: '3' },
+  // ─────────────────────────────
+  // Generation 2 – Kinder (1A–1E) + Partner
+  // ─────────────────────────────
+  { id: 'p3',  code: '1A',  name: 'Heinrich Geppert', gender: 'm', birthDate: '1954-02-11', birthPlace: 'Hamburg', parentId: 'p1', partnerId: 'p4', hasRing: false, ringCode: 'R02' },
+  { id: 'p4',  code: '1Ax', name: 'Eva Müller-Geppert', gender: 'f', birthDate: '1956-06-30', birthPlace: 'Bonn',   parentId: null, partnerId: 'p3', hasRing: true,  ringCode: 'R12' },
 
-  { id: '5', code: '1.2', name: 'Elisabeth Geppert', birthDate: '1948-06-01', parentId: '1', partnerId: '6' },
-  { id: '6', code: '1.2x', name: 'Johann Geppert', birthDate: '1947-12-12', parentId: null, partnerId: '5' },
+  { id: 'p5',  code: '1B',  name: 'Liselotte Geppert', gender: 'f', birthDate: '1952-10-21', birthPlace: 'Köln',   parentId: 'p1', partnerId: 'p6', hasRing: false, ringCode: 'R03' },
+  { id: 'p6',  code: '1Bx', name: 'Fritz Wagner-Geppert', gender: 'm', birthDate: '1951-01-13', birthPlace: 'Düsseldorf', parentId: null, partnerId: 'p5', hasRing: true,  ringCode: 'R13' },
 
-  { id: '7', code: '1.3', name: 'Peter Geppert', birthDate: '1953-08-09', parentId: '1', partnerId: '8' },
-  { id: '8', code: '1.3x', name: 'Clara Meyer-Geppert', birthDate: '1955-01-30', parentId: null, partnerId: '7' },
+  { id: 'p7',  code: '1C',  name: 'Otto Geppert', gender: 'm', birthDate: '1957-07-03', birthPlace: 'München', parentId: 'p1', partnerId: 'p8', hasRing: false, ringCode: 'R04' },
+  { id: 'p8',  code: '1Cx', name: 'Ingrid Becker-Geppert', gender: 'f', birthDate: '1959-03-28', birthPlace: 'Augsburg', parentId: null, partnerId: 'p7', hasRing: true,  ringCode: 'R14' },
 
-  { id: '9', code: '1.4', name: 'Monika Geppert', birthDate: '1951-04-17', parentId: '1', partnerId: '10' },
-  { id: '10', code: '1.4x', name: 'Franz Geppert', birthDate: '1949-07-23', parentId: null, partnerId: '9' },
+  { id: 'p9',  code: '1D',  name: 'Ruth Geppert', gender: 'f', birthDate: '1955-12-17', birthPlace: 'Stuttgart', parentId: 'p1', partnerId: 'p10', hasRing: false, ringCode: 'R05' },
+  { id: 'p10', code: '1Dx', name: 'Karl-Heinz Meyer-Geppert', gender: 'm', birthDate: '1954-08-09', birthPlace: 'Nürnberg', parentId: null, partnerId: 'p9', hasRing: true,  ringCode: 'R15' },
 
-  // Generation 3 – Enkel (Kinder von Hans & Maria)
-  { id: '11', code: '1.1.1', name: 'Thomas Geppert', birthDate: '1975-03-05', parentId: '3', partnerId: '12' },
-  { id: '12', code: '1.1.1x', name: 'Sabine Schulz-Geppert', birthDate: '1976-09-15', parentId: null, partnerId: '11' },
+  { id: 'p11', code: '1E',  name: 'Gerhard Geppert', gender: 'm', birthDate: '1960-05-05', birthPlace: 'Hannover', parentId: 'p1', partnerId: 'p12', hasRing: false, ringCode: 'R06' },
+  { id: 'p12', code: '1Ex', name: 'Ute Hoffmann-Geppert', gender: 'f', birthDate: '1961-11-23', birthPlace: 'Mainz', parentId: null, partnerId: 'p11', hasRing: true,  ringCode: 'R16' },
 
-  { id: '13', code: '1.1.2', name: 'Julia Geppert', birthDate: '1978-11-22', parentId: '3', partnerId: '14' },
-  { id: '14', code: '1.1.2x', name: 'Michael Geppert', birthDate: '1974-02-02', parentId: null, partnerId: '13' },
+  // ─────────────────────────────
+  // Generation 3 – Enkel (je 3 Kinder pro Linie) + Partner
+  // 1A-Kinder
+  { id: 'p13', code: '1A1',  name: 'Thomas Geppert', gender: 'm', birthDate: '1979-01-14', birthPlace: 'Hamburg', parentId: 'p3', partnerId: 'p14', hasRing: true,  ringCode: 'R01', inheritedFrom: '1' },
+  { id: 'p14', code: '1A1x', name: 'Sabine Schulz-Geppert', gender: 'f', birthDate: '1980-10-02', birthPlace: 'Bremen', parentId: null, partnerId: 'p13', hasRing: true, ringCode: 'R17' },
 
-  // Enkel (Kinder von Elisabeth & Johann)
-  { id: '15', code: '1.2.1', name: 'Andreas Geppert', birthDate: '1970-07-08', parentId: '5', partnerId: '16' },
-  { id: '16', code: '1.2.1x', name: 'Katrin Hoffmann-Geppert', birthDate: '1972-03-19', parentId: null, partnerId: '15' },
+  { id: 'p15', code: '1A2',  name: 'Julia Geppert', gender: 'f', birthDate: '1981-04-06', birthPlace: 'Kiel', parentId: 'p3', partnerId: 'p16', hasRing: false, ringCode: 'R07' },
+  { id: 'p16', code: '1A2x', name: 'Michael Weber-Geppert', gender: 'm', birthDate: '1978-07-20', birthPlace: 'Flensburg', parentId: null, partnerId: 'p15', hasRing: true, ringCode: 'R18' },
 
-  { id: '17', code: '1.2.2', name: 'Martina Geppert', birthDate: '1973-05-14', parentId: '5', partnerId: '18' },
-  { id: '18', code: '1.2.2x', name: 'Stefan Geppert', birthDate: '1971-09-25', parentId: null, partnerId: '17' },
+  { id: 'p17', code: '1A3',  name: 'Markus Geppert', gender: 'm', birthDate: '1983-02-18', birthPlace: 'Lübeck', parentId: 'p3', partnerId: 'p18', hasRing: true, ringCode: 'R19' },
+  { id: 'p18', code: '1A3x', name: 'Daniela Hoffmann-Geppert', gender: 'f', birthDate: '1984-06-12', birthPlace: 'Potsdam', parentId: null, partnerId: 'p17', hasRing: true, ringCode: 'R20' },
 
-  // Enkel (Kinder von Peter & Clara)
-  { id: '19', code: '1.3.1', name: 'Markus Geppert', birthDate: '1979-01-11', parentId: '7', partnerId: '20' },
-  { id: '20', code: '1.3.1x', name: 'Daniela Weber-Geppert', birthDate: '1980-10-10', parentId: null, partnerId: '19' },
+  // 1B-Kinder
+  { id: 'p19', code: '1B1',  name: 'Martina Geppert', gender: 'f', birthDate: '1976-05-01', birthPlace: 'Köln', parentId: 'p5', partnerId: 'p20', hasRing: false, ringCode: 'R08' },
+  { id: 'p20', code: '1B1x', name: 'Stefan Wagner-Geppert', gender: 'm', birthDate: '1975-09-16', birthPlace: 'Düsseldorf', parentId: null, partnerId: 'p19', hasRing: true, ringCode: 'R21' },
 
-  { id: '21', code: '1.3.2', name: 'Susanne Geppert', birthDate: '1977-06-06', parentId: '7', partnerId: '22' },
-  { id: '22', code: '1.3.2x', name: 'Andreas Müller-Geppert', birthDate: '1976-12-24', parentId: null, partnerId: '21' },
+  { id: 'p21', code: '1B2',  name: 'Andreas Geppert', gender: 'm', birthDate: '1978-03-10', birthPlace: 'Frankfurt am Main', parentId: 'p5', partnerId: 'p22', hasRing: true, ringCode: 'R03', inheritedFrom: '1B' },
+  { id: 'p22', code: '1B2x', name: 'Katrin Müller-Geppert', gender: 'f', birthDate: '1980-01-27', birthPlace: 'Wiesbaden', parentId: null, partnerId: 'p21', hasRing: true, ringCode: 'R22' },
 
-  // Enkel (Kinder von Monika & Franz)
-  { id: '23', code: '1.4.1', name: 'Christian Geppert', birthDate: '1972-08-02', parentId: '9', partnerId: '24' },
-  { id: '24', code: '1.4.1x', name: 'Birgit Schneider-Geppert', birthDate: '1974-01-18', parentId: null, partnerId: '23' },
+  { id: 'p23', code: '1B3',  name: 'Heike Geppert', gender: 'f', birthDate: '1982-08-24', birthPlace: 'Mainz', parentId: 'p5', partnerId: 'p24', hasRing: true, ringCode: 'R23' },
+  { id: 'p24', code: '1B3x', name: 'Matthias Becker-Geppert', gender: 'm', birthDate: '1981-12-02', birthPlace: 'Darmstadt', parentId: null, partnerId: 'p23', hasRing: true, ringCode: 'R24' },
 
-  { id: '25', code: '1.4.2', name: 'Heike Geppert', birthDate: '1976-09-30', parentId: '9', partnerId: '26' },
-  { id: '26', code: '1.4.2x', name: 'Matthias Geppert', birthDate: '1975-04-04', parentId: null, partnerId: '25' },
+  // 1C-Kinder
+  { id: 'p25', code: '1C1',  name: 'Peter Geppert', gender: 'm', birthDate: '1985-11-11', birthPlace: 'München', parentId: 'p7', partnerId: 'p26', hasRing: true, ringCode: 'R04', inheritedFrom: '1C' },
+  { id: 'p26', code: '1C1x', name: 'Claudia Schulz-Geppert', gender: 'f', birthDate: '1987-03-08', birthPlace: 'Regensburg', parentId: null, partnerId: 'p25', hasRing: true, ringCode: 'R25' },
 
-  // Generation 4 – Urenkel (Kinder von Thomas & Sabine)
-  { id: '27', code: '1.1.1.1', name: 'Lukas Geppert', birthDate: '2000-02-14', parentId: '11' },
-  { id: '28', code: '1.1.1.2', name: 'Lea Geppert', birthDate: '2002-11-05', parentId: '11' },
+  { id: 'p27', code: '1C2',  name: 'Susanne Geppert', gender: 'f', birthDate: '1986-07-13', birthPlace: 'Augsburg', parentId: 'p7', partnerId: 'p28', hasRing: false, ringCode: 'R09' },
+  { id: 'p28', code: '1C2x', name: 'Andreas Meyer-Geppert', gender: 'm', birthDate: '1984-10-29', birthPlace: 'Ingolstadt', parentId: null, partnerId: 'p27', hasRing: true, ringCode: 'R26' },
 
-  // Kinder von Julia & Michael
-  { id: '29', code: '1.1.2.1', name: 'David Geppert', birthDate: '1999-03-20', parentId: '13' },
-  { id: '30', code: '1.1.2.2', name: 'Clara Geppert', birthDate: '2001-07-12', parentId: '13' },
+  { id: 'p29', code: '1C3',  name: 'Johannes Geppert', gender: 'm', birthDate: '1988-01-22', birthPlace: 'Rosenheim', parentId: 'p7', partnerId: 'p30', hasRing: true, ringCode: 'R27' },
+  { id: 'p30', code: '1C3x', name: 'Nina Hoffmann-Geppert', gender: 'f', birthDate: '1989-09-07', birthPlace: 'Passau', parentId: null, partnerId: 'p29', hasRing: true, ringCode: 'R28' },
 
-  // Kinder von Andreas & Katrin
-  { id: '31', code: '1.2.1.1', name: 'Johannes Geppert', birthDate: '1998-09-09', parentId: '15' },
-  { id: '32', code: '1.2.1.2', name: 'Sarah Geppert', birthDate: '2003-05-25', parentId: '15' },
+  // 1D-Kinder
+  { id: 'p31', code: '1D1',  name: 'Christian Geppert', gender: 'm', birthDate: '1980-05-30', birthPlace: 'Stuttgart', parentId: 'p9', partnerId: 'p32', hasRing: true, ringCode: 'R05', inheritedFrom: '1D' },
+  { id: 'p32', code: '1D1x', name: 'Birgit Schneider-Geppert', gender: 'f', birthDate: '1981-02-19', birthPlace: 'Heilbronn', parentId: null, partnerId: 'p31', hasRing: true, ringCode: 'R29' },
 
-  // Kinder von Martina & Stefan
-  { id: '33', code: '1.2.2.1', name: 'Paul Geppert', birthDate: '1996-04-01', parentId: '17' },
-  { id: '34', code: '1.2.2.2', name: 'Anna Geppert', birthDate: '1997-08-18', parentId: '17' },
+  { id: 'p33', code: '1D2',  name: 'Markus Geppert', gender: 'm', birthDate: '1983-06-21', birthPlace: 'Tübingen', parentId: 'p9', partnerId: 'p34', hasRing: false, ringCode: 'R10' },
+  { id: 'p34', code: '1D2x', name: 'Andrea Weber-Geppert', gender: 'f', birthDate: '1984-12-14', birthPlace: 'Ulm', parentId: null, partnerId: 'p33', hasRing: true, ringCode: 'R30' },
 
-  // Kinder von Markus & Daniela
-  { id: '35', code: '1.3.1.1', name: 'Finn Geppert', birthDate: '2004-12-03', parentId: '19' },
-  { id: '36', code: '1.3.1.2', name: 'Mia Geppert', birthDate: '2006-07-22', parentId: '19' },
+  { id: 'p35', code: '1D3',  name: 'Heike Geppert', gender: 'f', birthDate: '1986-09-18', birthPlace: 'Karlsruhe', parentId: 'p9', partnerId: 'p36', hasRing: true, ringCode: 'R31' },
+  { id: 'p36', code: '1D3x', name: 'Alex Braun-Geppert', gender: 'd', birthDate: '1987-04-03', birthPlace: 'Mannheim', parentId: null, partnerId: 'p35', hasRing: true, ringCode: 'R32' },
 
-  // Kinder von Susanne & Andreas
-  { id: '37', code: '1.3.2.1', name: 'Leon Geppert', birthDate: '1995-01-15', parentId: '21' },
-  { id: '38', code: '1.3.2.2', name: 'Nina Geppert', birthDate: '1999-10-29', parentId: '21' },
+  // 1E-Kinder
+  { id: 'p37', code: '1E1',  name: 'Philipp Geppert', gender: 'm', birthDate: '1987-08-26', birthPlace: 'Hannover', parentId: 'p11', partnerId: 'p38', hasRing: true, ringCode: 'R06', inheritedFrom: '1E' },
+  { id: 'p38', code: '1E1x', name: 'Sophie Müller-Geppert', gender: 'f', birthDate: '1989-01-08', birthPlace: 'Celle', parentId: null, partnerId: 'p37', hasRing: true, ringCode: 'R33' },
 
-  // Kinder von Christian & Birgit
-  { id: '39', code: '1.4.1.1', name: 'Jonas Geppert', birthDate: '1997-06-16', parentId: '23' },
-  { id: '40', code: '1.4.1.2', name: 'Sophie Geppert', birthDate: '2001-12-20', parentId: '23' },
+  { id: 'p39', code: '1E2',  name: 'Nadine Geppert', gender: 'f', birthDate: '1990-11-09', birthPlace: 'Hildesheim', parentId: 'p11', partnerId: 'p40', hasRing: true, ringCode: 'R34' },
+  { id: 'p40', code: '1E2x', name: 'Daniel Hoffmann-Geppert', gender: 'm', birthDate: '1989-05-15', birthPlace: 'Göttingen', parentId: null, partnerId: 'p39', hasRing: true, ringCode: 'R35' },
 
-  // Kinder von Heike & Matthias
-  { id: '41', code: '1.4.2.1', name: 'Emil Geppert', birthDate: '1998-02-08', parentId: '25' },
-  { id: '42', code: '1.4.2.2', name: 'Marie Geppert', birthDate: '2000-09-17', parentId: '25' },
+  { id: 'p41', code: '1E3',  name: 'Fabian Geppert', gender: 'm', birthDate: '1992-03-02', birthPlace: 'Braunschweig', parentId: 'p11', partnerId: 'p42', hasRing: true, ringCode: 'R36' },
+  { id: 'p42', code: '1E3x', name: 'Ulrike Schulz-Geppert', gender: 'f', birthDate: '1993-12-30', birthPlace: 'Wolfsburg', parentId: null, partnerId: 'p41', hasRing: true, ringCode: 'R37' },
 
-  // Generation 5 – Ururenkel (z. B. Lukas' Kinder)
-  { id: '43', code: '1.1.1.1.1', name: 'Ben Geppert', birthDate: '2020-04-11', parentId: '27' },
-  { id: '44', code: '1.1.1.1.2', name: 'Lia Geppert', birthDate: '2022-06-30', parentId: '27' },
+  // ─────────────────────────────
+  // Generation 4 – Urenkel (8 Kinder, ohne Partner) + Vererbungen
+  // ─────────────────────────────
+  { id: 'p43', code: '1A11', name: 'Lukas Geppert',   gender: 'm', birthDate: '2006-02-14', birthPlace: 'Hamburg', parentId: 'p13', partnerId: null, hasRing: true,  ringCode: 'R02', inheritedFrom: '1A' },
+  { id: 'p44', code: '1A12', name: 'Lea Geppert',     gender: 'f', birthDate: '2008-11-05', birthPlace: 'Hamburg', parentId: 'p13', partnerId: null, hasRing: true,  ringCode: 'R07', inheritedFrom: '1A2' },
 
-  // Weitere Ururenkel
-  { id: '45', code: '1.1.2.1.1', name: 'Milan Geppert', birthDate: '2018-03-05', parentId: '29' },
-  { id: '46', code: '1.2.1.1.1', name: 'Ella Geppert', birthDate: '2019-07-14', parentId: '31' },
-  { id: '47', code: '1.2.2.1.1', name: 'Noah Geppert', birthDate: '2017-12-01', parentId: '33' },
-  { id: '48', code: '1.3.1.1.1', name: 'Clara-Marie Geppert', birthDate: '2016-05-25', parentId: '35' },
-  { id: '49', code: '1.3.2.1.1', name: 'Luis Geppert', birthDate: '2015-09-10', parentId: '37' },
-  { id: '50', code: '1.4.2.1.1', name: 'Marta Geppert', birthDate: '2021-11-19', parentId: '41' },
+  { id: 'p45', code: '1B21', name: 'David Geppert',   gender: 'm', birthDate: '2011-03-20', birthPlace: 'Frankfurt am Main', parentId: 'p21', partnerId: null, hasRing: true, ringCode: 'R08', inheritedFrom: '1B1' },
+  { id: 'p46', code: '1C11', name: 'Clara Geppert',   gender: 'f', birthDate: '2010-07-12', birthPlace: 'München', parentId: 'p25', partnerId: null, hasRing: true, ringCode: 'R09', inheritedFrom: '1C2' },
+
+  { id: 'p47', code: '1D31', name: 'Jonas Geppert',   gender: 'm', birthDate: '2012-05-16', birthPlace: 'Karlsruhe', parentId: 'p35', partnerId: null, hasRing: true, ringCode: 'R10', inheritedFrom: '1D2' },
+  { id: 'p48', code: '1E21', name: 'Sophie Geppert',  gender: 'f', birthDate: '2013-12-20', birthPlace: 'Hildesheim', parentId: 'p39', partnerId: null, hasRing: true, ringCode: 'R15', inheritedFrom: '1Dx' },
+
+  { id: 'p49', code: '1A21', name: 'Paul Geppert',    gender: 'm', birthDate: '2014-04-11', birthPlace: 'Kiel', parentId: 'p15', partnerId: null, hasRing: true, ringCode: 'R38' },
+  { id: 'p50', code: '1C31', name: 'Mira Geppert',    gender: 'd', birthDate: '2015-09-10', birthPlace: 'Rosenheim', parentId: 'p29', partnerId: null, hasRing: true, ringCode: 'R39' },
 ];
